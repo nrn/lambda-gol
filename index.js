336b90dd2f1135fa82722d950311cc8a4a7ccc30
Nick Niemeir <nick.niemeir@gmail.com>
2014-11-16 13:42:47 -0800

    Better fn names, group fns, cleanup display logic

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // Seed values as a list of x,y pairs: ( ( x, y), (x, y) )
-var next = cons(cons(num1, num2), cons(cons(num2, num2), cons(cons(num3, num2), nil)))
+// blinker
+// var next = cons(cons(num1, num2), cons(cons(num2, num2), cons(cons(num3, num2), nil)))
+// toad
+// var next = cons(cons(num1, num2), cons(cons(num2, num2), cons(cons(num3, num2), cons(cons(num2, num3), cons(cons(num3, num3), cons(cons(num4, num3), nil))))))
+// glider
+var next = cons(cons(num3, num1), cons(cons(num4, num2), cons(cons(num2, num3), cons(cons(num3, num3), cons(cons(num4, num3), nil)))))
 
-console.log(display(next))
+display(next)
 
 setInterval(function () {
   next = start(next)
-  console.log(display(next))
+  display(next)
 }, 1000)
 
 function start (last) {
@@ -17,7 +22,7 @@ function filter (neighbors, last, living, seen) {
     return living
   }, function () {
     return (function (cell) {
-      return lif(mem(cell, seen), function () {
+      return lif(member(cell, seen), function () {
         return filter(cdr(neighbors), last, living, seen)
       }, function () {
         return (function (tot) {
@@ -25,7 +30,7 @@ function filter (neighbors, last, living, seen) {
             return filter(cdr(neighbors), last, cons(cell, living), cons(cell, seen))
           }, function () {
             return lif(eq(tot,num2), function () {
-              return lif(mem(cell, last), function () {
+              return lif(member(cell, last), function () {
                 return filter(cdr(neighbors), last, cons(cell, living), cons(cell, seen))
               }, function () {
                 return filter(cdr(neighbors), last, living, cons(cell, seen))
@@ -34,25 +39,25 @@ function filter (neighbors, last, living, seen) {
               return filter(cdr(neighbors), last, living, cons(cell, seen))
             })()
           })()
-        })(sum(neighbors, cell, num0))
+        })(howMany(neighbors, cell, num0))
       })()
     })(car(neighbors))
   })()
 }
 
-function mem (cell, list) {
-  return lif(eq(sum(list, cell, num0), num0), f, t)
+function member (cell, list) {
+  return lif(eq(howMany(list, cell, num0), num0), f, t)
 }
 
-function sum (list, cell, tot) {
+function howMany (list, cell, tot) {
   return lif(nul(list), function () {
     return tot
   }, function () {
-    return sum(cdr(list), cell, lif(same(car(list), cell), inc(tot), tot))
+    return howMany(cdr(list), cell, lif(samePoint(car(list), cell), inc(tot), tot))
   })()
 }
 
-function same (a, b) {
+function samePoint (a, b) {
   return lif(and(eq(car(a), car(b)), eq(cdr(a), cdr(b))), t, f)
 }
 
@@ -73,6 +78,8 @@ function listNeighbors (l) {
   })()
 }
 
+// lists
+
 function cons (a, b) {
   return function (op) {
     return op(a, b)
@@ -87,6 +94,16 @@ function cdr (a) {
   return a(f)
 }
 
+function nil () {
+  return t
+}
+
+function nul (p) {
+  return p(makeConst(f))
+}
+
+// logic
+
 function t (a, b) {
   return a
 }
@@ -99,9 +116,59 @@ function lif (a, b, c) {
   return a(b, c)
 }
 
-function nil () {
-  return t
+function and (a, b) {
+  return a(b, f)
 }
+
+// maths
+
+function eq (a, b) {
+  return lif(and(isZero(sub(a, b)), isZero(sub(b, a))), t, f)
+}
+
+function inc (n) {
+  return function (f) {
+    return function (x) {
+      return f(n(f)(x))
+    }
+  }
+}
+
+function isZero (n) {
+  return n(makeConst(f))(t)
+}
+
+function dec (n) {
+  return function (f) {
+    return function (x) {
+      return n(function (g) { return function (h) { return h(g(f)) }}
+        )(makeConst(x)
+        )(id)
+    }
+  }
+}
+
+function sub (a, b) {
+  return b(dec)(a)
+}
+
+function add (a, b) {
+  return b(inc)(a)
+}
+
+// util
+
+function id (x) {
+  return x
+}
+
+function makeConst (x) {
+  return function (_) {
+    return x
+  }
+}
+
+// num literals for seed values
 
 function num0 (_) {
   return id
@@ -131,73 +198,21 @@ function num4 (f) {
   }
 }
 
-function nul (p) {
-  return p(makeConst(f))
-}
-
-function and (a, b) {
-  return a(b, f)
-}
-
-function eq (a, b) {
-  return lif(and(isZero(sub(a, b)), isZero(sub(b, a))), t, f)
-}
-
-function inc (n) {
-  return function (f) {
-    return function (x) {
-      return f(n(f)(x))
-    }
-  }
-}
-
-function isZero (n) {
-  return n(makeConst(f))(t)
-}
-
-
-function dec (n) {
-  return function (f) {
-    return function (x) {
-      return n(function (g) { return function (h) { return h(g(f)) }}
-        )(makeConst(x)
-        )(id)
-    }
-  }
-}
-
-function sub (a, b) {
-  return b(dec)(a)
-}
-
-function id (x) {
-  return x
-}
-
-function makeConst (x) {
-  return function (_) {
-    return x
-  }
-}
-
 // display logic
 
 function display (l) {
+  console.log(_display(l, '( '))
+}
+
+function _display (l, sofar) {
   return lif(nul(l), function () {
-    return ''
+    return sofar + ')'
   }, function () {
-    return '( ' + num(car(car(l))) + ', ' + num(cdr(car(l))) + ' ), ' + display(cdr(l))
+    return _display(cdr(l), sofar + '( ' + num(car(car(l))) + ', ' + num(cdr(car(l))) + ' ) ')
   })()
 }
 
 function num (n) {
-  return displayNum(n, 0)
+  return n(function (x) { return x + 1 })(0)
 }
 
-function displayNum (n, total) {
-  return lif(isZero(n), function () {
-    return total
-  }, function () {
-    return displayNum(dec(n), total+1)
-  })()
-}
